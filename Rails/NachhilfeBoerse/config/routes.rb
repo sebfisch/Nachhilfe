871e3e79233a6a672b8f68bbb46faadffff24346
Sebastian Fischer <mail@sebfisch.de>
2014-06-17 16:48:52 +0200

    fixed duplicate route name

--- Rails/NachhilfeBoerse/config/routes.rb
+++ Rails/NachhilfeBoerse/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get "users/:id" => "users#get", :as => :users
 
-  delete "offers/:id" => "users#delete_offer", :as => :offer
+  delete "offers/:id" => "users#delete_offer"
   delete "demand/:id" => "users#delete_demand", :as => :demand
 
 
