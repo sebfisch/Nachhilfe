8f11aa727b8f8a76c427f38bff7f80d0e455bdf0
Christine Boetefuehr <boetefuehr@aol.com>
2014-06-17 15:48:19 +0200

    Nutzerprofil anzeigen

--- Rails/NachhilfeBoerse/config/routes.rb
+++ Rails/NachhilfeBoerse/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+ get "users/:id" => "users#get", :as => :users
+
+  delete "offers/:id" => "users#delete_offer", :as => :offer
+  delete "demand/:id" => "users#delete_demand", :as => :demand
 
 end
