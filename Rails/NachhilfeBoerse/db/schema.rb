11e24b3eae32df1aede947b70ae855a8fa3545c7
Claudia <knuetel@online.de>
2014-06-24 12:24:47 +0200

    Navigationsleiste

--- Rails/NachhilfeBoerse/db/schema.rb
+++ Rails/NachhilfeBoerse/db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "users", force: true do |t|
     t.string  "name"
-    t.string  "password"
     t.integer "class_level"
     t.string  "contact_info"
     t.string  "image_link"
