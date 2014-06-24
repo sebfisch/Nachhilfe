c81485d2d1c06430359c90af603db2f44cfc268e
ThomasRief <thomasriefle@gmx.de>
2014-06-24 13:55:09 +0200

    Admin angasst

--- Rails/NachhilfeBoerse/app/controllers/admin_controller.rb
+++ Rails/NachhilfeBoerse/app/controllers/admin_controller.rb
@@ -1,5 +1,8 @@
 class AdminController < ApplicationController
     def get
+	  if current_user != nil then
+	    @is_admin = current_user.is_admin
+	  end	
 	  @users = User.all
 	  @new_user = User.new
     end
